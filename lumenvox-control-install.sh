e06c8a904202b92ab5685ebb35a5eaf7b21a96f5
LumenVoxCloudTeam <JacksonBriones@lumenvox.com>
2025-01-15 15:34:30 -0500

    add version for ingress-nginx chart

--- lumenvox-control-install.sh
+++ lumenvox-control-install.sh
@@ -919,7 +919,7 @@ fi
 #############################################
 
 printf "9. Installing nginx ingress controller...\n" | $TEE -a
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx -n ingress-nginx --create-namespace --set controller.hostNetwork=true --set controller.allowSnippetAnnotations=true 1>>$MAIN_LOG 2>>$ERR_LOG
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx -n ingress-nginx --create-namespace --set controller.hostNetwork=true --version 4.11.3 --set controller.allowSnippetAnnotations=true 1>>$MAIN_LOG 2>>$ERR_LOG
 if [ $? -ne 0 ]; then
     printf "\t\tFailed to install nginx ingress controller\n" | $TEE -a
     exit 1
